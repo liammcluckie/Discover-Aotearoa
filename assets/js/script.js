a85753b2ea6ab442e97efad38b743bb93fd2d236
liammcluckie <liammcluckie@hotmail.co.uk>
2021-02-11 11:26:37 +0000

    WIP imported jquery for flickity carousel template

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+//Popular Destinations Carousel
+//Code taken from https://flickity.metafizzy.co/ for image carousel
+
+$('.main-carousel').flickity({
+  // options
+  cellAlign: 'left',
+  contain: true
+});
+
