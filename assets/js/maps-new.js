b42ca5bb8411a6f4cfc939467ed16efbb5ad4385
liammcluckie <liammcluckie@hotmail.co.uk>
2021-03-08 10:38:12 +0000

    Refactor map functions and marker code

--- assets/js/maps-new.js
+++ assets/js/maps-new.js
@@ -1,6 +1,6 @@
 //Set default map location
 function initMap() {
-    map = new google.maps.Map(document.getElementById("ni-dest-map"), {
+    const map = new google.maps.Map(document.getElementById("ni-dest-map"), {
         zoom: 4,
         disableDefaultUI: true,
         center: {
@@ -11,9 +11,9 @@ function initMap() {
 };
 
 //Set map to Wellington
-document.getElementById("wellington").addEventListener("click", initMapWell);
+document.getElementById("wellington").addEventListener("click", mapWell);
 
-function initMapWell() {
+function mapWell() {
     const wellington = { lat: -41.29235450909053, lng: 174.77948265394016 }; //Wellington
     const map = new google.maps.Map(document.getElementById("ni-dest-map"), {
         zoom: 14,
@@ -30,18 +30,18 @@ function initMapWell() {
 document.getElementById("welly-rest").addEventListener("click", wellRestaurants);
 
 function wellRestaurants() {
-    var restMap = new google.maps.Map(document.getElementById("ni-dest-map"), {
-        zoom: 15,
+    const restMap = new google.maps.Map(document.getElementById("ni-dest-map"), {
+        zoom: 16,
         center: {
             lat: -41.29235450909053, 
             lng: 174.77948265394016
         }
     });
-    let labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let labels = "123";
     let wellRestLocation = [
-        { lat: -41.2950004986167, lng: 174.78421241048974 }, //La Boca Loca
+        { lat: -41.2951073042275, lng: 174.78356063375207 }, //Boquita
         { lat: -41.291911555689836, lng: 174.77529899514886 }, //Little Penang
-        { lat: -41.29436051901506, lng: 174.7751597004475 } //Olive
+        { lat: -41.2943642230102, lng: 174.77507513229375 } //Olive 
     ];
     let markers = wellRestLocation.map(function (location, i) {
         return new google.maps.Marker({
@@ -50,6 +50,64 @@ function wellRestaurants() {
         });
     });
     new MarkerClusterer(restMap, markers, {
+        imagePath:
+            "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+    });
+}
+
+//Set map to Wellington Accommodation
+document.getElementById("welly-accomm").addEventListener("click", wellAccommodation);
+
+function wellAccommodation() {
+    const accommMap = new google.maps.Map(document.getElementById("ni-dest-map"), {
+        zoom: 16,
+        center: {
+            lat: -41.29235450909053, 
+            lng: 174.77948265394016
+        }
+    });
+    let labels = "123";
+    let wellAccommLocation = [
+        { lat: -41.29170651146583, lng: 174.78272230164237 }, //QT Wellington
+        { lat: -41.29152858791054, lng: 174.78076806605986 }, //U Residence Hotel
+        { lat: -41.295207298517475, lng: 174.7750301343776 } //CQ Hotels
+    ];
+    let markers = wellAccommLocation.map(function (location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+    new MarkerClusterer(accommMap, markers, {
+        imagePath:
+            "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+    });
+}
+
+//Set map to Wellington Attractions
+document.getElementById("welly-attr").addEventListener("click", wellAttractions);
+
+function wellAttractions() {
+    const attrMap = new google.maps.Map(document.getElementById("ni-dest-map"), {
+        zoom: 14,
+        center: {
+            lat: -41.29235450909053, 
+            lng: 174.77948265394016
+        }
+    });
+    let labels = "123";
+    let wellAttrLocation = [
+        { lat: -41.28420510741158, lng: 174.7752955550159 }, //Cable Car
+        { lat: -41.29042705680073, lng: 174.782306656861 }, //Te Papa Museum
+        { lat: -41.29600266967774, lng: 174.79464299183735 } //Mt Victoria
+    ];
+    let markers = wellAttrLocation.map(function (location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+    new MarkerClusterer(attrMap, markers, {
         imagePath:
             "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
     });
