d09654586ee38dcbc087f03b80040a0d767febfa
liammcluckie <liammcluckie@hotmail.co.uk>
2021-03-08 12:17:07 +0000

    Add martinborough map and marker functions

--- assets/js/maps-new.js
+++ assets/js/maps-new.js
@@ -26,6 +26,13 @@ function mapWell() {
     });
 }
 
+//Display Wellington top picks info
+document.getElementById("wellington").addEventListener("click", wellingtonPicks);
+function wellingtonPicks() {
+    let recommendWellington = document.getElementById("recommend-wellington")
+    recommendWellington.style.display = "block";
+}
+
 //Set map to Wellington Restaurants
 document.getElementById("welly-rest").addEventListener("click", wellRestaurants);
 
@@ -102,6 +109,116 @@ function wellAttractions() {
         { lat: -41.29600266967774, lng: 174.79464299183735 } //Mt Victoria
     ];
     let markers = wellAttrLocation.map(function (location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+    new MarkerClusterer(attrMap, markers, {
+        imagePath:
+            "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+    });
+}
+
+//Set map to Martinborough
+document.getElementById("martinborough").addEventListener("click", mapMart);
+
+function mapMart() {
+    const martinborough = { lat: -41.21998622909235, lng: 175.46008741862477 }; //Martinborough
+    const map = new google.maps.Map(document.getElementById("ni-dest-map"), {
+        zoom: 15,
+        center: martinborough
+    });
+    const marker = new google.maps.Marker({
+        position: martinborough,
+        map: map,
+        animation: google.maps.Animation.DROP
+    });
+}
+
+//Display Martinborough top picks info
+document.getElementById("martinborough").addEventListener("click", martinboroughPicks);
+function martinboroughPicks() {
+    let recommendMartinborough = document.getElementById("recommend-martinborough")
+    recommendMartinborough.style.display = "block";
+}
+
+//Set map to Martinborough Restaurants
+document.getElementById("mart-rest").addEventListener("click", martRestaurant);
+
+function martRestaurant() {
+    const restMap = new google.maps.Map(document.getElementById("ni-dest-map"), {
+        zoom: 14,
+        center: {
+            lat: -41.21998622909235, //Martinborough 
+            lng: 175.46008741862477
+        }
+    });
+    let labels = "123";
+    let martRestLocation = [
+        { lat: -41.216266185883725, lng: 175.46540193231985 }, //Cambridge Road
+        { lat: -41.22337633503677, lng: 175.47515828877738 }, //Colombo
+        { lat: -41.21697262978785, lng: 175.48549768940939 } //Luna
+    ];
+    let markers = martRestLocation.map(function (location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+    new MarkerClusterer(restMap, markers, {
+        imagePath:
+            "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+    });
+}
+
+//Set map to Martinborough Accommodation
+document.getElementById("mart-accomm").addEventListener("click", martAccommodation);
+
+function martAccommodation() {
+    const accommMap = new google.maps.Map(document.getElementById("ni-dest-map"), {
+        zoom: 14,
+        center: {
+            lat: -41.215587892705436, //Martinborough 
+            lng: 175.45813476983486
+        }
+    });
+    let labels = "123";
+    let martAccommLocation = [
+        { lat: -41.20833484931799, lng: 175.45729143969933 }, //Peppers
+        { lat: -41.218272140033925, lng: 175.4592038427573 }, //Mart Hotel
+        { lat: -41.21140356803334, lng: 175.46654030901948 } //Aylestone
+    ];
+    let markers = martAccommLocation.map(function (location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+    new MarkerClusterer(accommMap, markers, {
+        imagePath:
+            "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+    });
+}
+
+//Set map to Martinborough Attractions
+document.getElementById("mart-attr").addEventListener("click", martAttraction);
+
+function martAttraction() {
+    const attrMap = new google.maps.Map(document.getElementById("ni-dest-map"), {
+        zoom: 10,
+        center: {
+            lat: -41.32609427505324, //Martinborough -41.32609427505324, 175.4427779972823
+            lng: 175.4427779972823
+        }
+    });
+    let labels = "123";
+    let martAttrLocation = [
+        { lat: -41.21548669061603, lng: 175.45715036057425 }, //Wine Tours
+        { lat: -41.50600405919251, lng: 175.43036948388405 }, //Horse Trek
+        { lat: -41.27592440562157, lng: 175.4131342262065 } //Ranui
+    ];
+    let markers = martAttrLocation.map(function (location, i) {
         return new google.maps.Marker({
             position: location,
             label: labels[i % labels.length]
